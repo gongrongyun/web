c060d600a037df51dece6489f3cf72da6840b2c3
gongrongyun <1716175849@qq.com>
2019-08-19 21:49:47 +0800

    modify ShowBoard Component

--- src/Components/ShowBoard.js
+++ src/Components/ShowBoard.js
@@ -1,36 +1,75 @@
 import React from "react"
 import server from "../server";
 import Article from "./Article";
+import "./index.css";
+import { Card, Pagination, Empty } from "antd";
 
 class ShowBoard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             articles: [],
+            total: 0,
+            currentPage: 1,
+            loading: true,
         }
     }
 
     componentWillMount() {
-        const articleType = this.getArticleType();
+        this.getData();
+    }
+
+    componentWillReceiveProps() {
+        this.getData();
+    }
+
+    getData = () => {
         const params = {
-            offset: 0,
+            offset: (this.state.currentPage - 1) * 7,
             limit: 7,
         };
-        server.get(`article/list/${articleType}`, {params}).then(response => {
-            this.setState({articles: response.data || []});
+        server.get(`article/list/${this.props.articleType}`, {params}).then(response => {
+            this.setState({
+                articles: response.data.articles || [],
+                total: response.data.total,
+                loading: response.data.articles ? false : true,
+            });
         })
     }
 
-    getArticleType = () => {
-        return window.location.href.split("/")[4];
+    changePage = (pageNum) => {
+        this.setState({currentPage: pageNum}, function() {
+            this.getData();
+        })
     }
 
     render() {
+        const map = {
+            "all": "全部",
+            "comprehensive": "综合",
+            "study": "学习",
+            "life": "生活",
+            "technology": "科技",
+        }
+
         return(
             <div className="showBoard-container">
-                { this.state.articles.map((article) => (
-                    <Article key={ article.id } article={ article } visibilty={false} />
-                )) }
+                <Card 
+                    className="showBoard-board"
+                    title={ map[this.props.articleType] }
+                >
+                    { this.state.loading ? <Empty/>  : this.state.articles.map((article) => (
+                        <Article key={ article.id } article={ article } visibilty={false} />
+                    ))}
+                    <Pagination 
+                        className="pagination" 
+                        defaultCurrent={1} 
+                        showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+                        onChange={ this.changePage }
+                        pageSize={ 7 }
+                        total={ this.state.total } 
+                    />
+                </Card>
             </div>
         )
     }
